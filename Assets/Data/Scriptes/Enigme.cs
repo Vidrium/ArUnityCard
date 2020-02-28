7405af657748c1666a4633425f3688a8dc5a2b0c
Vivien <vivien.caux@outlook.fr>
2020-02-28 15:14:16 +0100

    Commit

---
+++ Assets/Data/Scriptes/Enigme.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+public class Enigme : MonoBehaviour
+{
+    public Dictionary<int, string> enigmes = new Dictionary<int, string>();
+    public List<int> enigmeMentioned = new List<int>();
+    public Text enigmeText;
+    public string enigme = "";
+
+    private int number;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enigmeMentioned.Add(1);
+        enigmeMentioned.Add(2);
+        enigmeMentioned.Add(3);
+        enigmes.Add(1, "Qu’est-ce qu’un homme peut porter mais que 10 hommes ne peuvent mettre debout ?");
+        enigmes.Add(2, "Qu’est ce qui se brise quand on me prononce ?");
+        enigmes.Add(3, "Qu'est-ce qui à des cités mais pas de maison, des forêts mais pas d'arbres et des rivières mais pas d'eau ?");
+
+        int random = RandomEnigme();
+        enigme = enigmes[random];
+        Debug.Log("enigme : " + enigme);
+        enigmeText.text = enigme;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    private int RandomEnigme()
+    {
+        number = Random.Range(1, 3);
+        bool notMentioned = enigmeMentioned.Contains(number);
+        if (notMentioned == true)
+        {
+            enigmeMentioned.Remove(number);
+        }
+
+        return number;
+    }
+
+
+}
---
+++ Assets/Data/Scriptes/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float temps = 60;
+    public Text timerText;
+    public int tempsInt;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tempsInt = Mathf.RoundToInt(temps);
+        timerText.text = "il vous reste: " + tempsInt + " secondes";
+        if (temps >= 0)
+        {
+            temps -= Time.deltaTime;
+        }
+    }
+}
